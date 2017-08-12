54d5d287637ffad780ee1992b52bdc1471078bf3
millsgerald <30513476+millsgerald@users.noreply.github.com>
2017-08-12 19:08:20 +1000

    Create sum.py
    Simple program to add two numbers together that are provided by the user

---
+++ sum.py
@@ -0,0 +1,4 @@
+# Simple program to add two numbers together that are provided by the user
+numone = input ('Enter a number between 1 and 10\n')
+numtwo = input ('Enter a number between 11 and 20\n')
+print (int(numone) + int(numtwo))
