0276bf76420c301925444b8c5d8e05cbfa485e03
millsgerald <30513476+millsgerald@users.noreply.github.com>
2017-08-19 16:07:06 +1000

    Create GradeTestWIP.py

---
+++ GradeTestWIP.py
@@ -0,0 +1,25 @@
+# Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message
+# If the score is between 0.0 and 1.0, print a grade using the following table
+# Score Grade >= 0.9 A >= 0.8 B >= 0.7 C >= 0.6 D < 0.6 F 
+# Enter score: perfect Bad score
+# Enter score: 10.0 Bad score
+# Enter score: 0.75 C
+# Enter score: 0.5 F
+# USES MULTI WAY BRANCH
+X = input('Enter a score between 0.0 and 1.0\n')
+try:
+    score = float(X)
+    score > 0 and score < 1
+    if score >= 0.9:
+        print('A')
+    elif score >=0.8:
+        print('B')
+    elif score >=0.7:
+        print('C')
+    elif score >=0.6:
+        print('D')
+    elif score <0.6:
+        print('F')
+except:
+    print('Bad Score')
+# I am trying to get the above to only run iof the number is between 0 and 1 or print bad score but I don't think we can use logical operators for this
