c336d24616ebf714326f864093a53685a7a5e80d
Gerald Mills <gerald.mills@outlook.com>
2017-08-22 10:52:39 +1000

    changes

---
+++ license.py
@@ -0,0 +1,12 @@
+# Write a python program simulating driver's license provision. Accept the age of the driver and his total number of practice
+#  hours. If driver’s age is below 16, do not issue license. if 16 or above then check if number of practice hours is 
+#  more than(ie. >) 200. if > 200, issue license otherwise don’t.
+
+age = int(input('Enter your age\n'))
+hours = int(input('Enter the number of practice hours\n'))
+if age < 16:
+	print ('you are to young to have a license')
+if hours < 200:
+	print(' you have not done enough practise hours')
+else:
+	print('Congratulations here is your license')
