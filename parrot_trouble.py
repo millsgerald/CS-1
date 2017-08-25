e56c08e9d59946d1e7f4182a13b0a1f154775141
Gerald Mills <gerald.mills@outlook.com>
2017-08-25 15:37:31 +1000

    Checkpoint 1 tests

--- parrot_trouble.py
+++ parrot_trouble.py
@@ -1,7 +1,11 @@
 #We have a loud talking parrot. The "hour" input is the current hour time in the range 0..23. 
 #We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble
-hour = (input('Enter the current hour? 0 thru 23.\n'))
-if (float(hour) >= 7) and (float(hour) <= 20):
+hour = input('Enter the current hour? 0 thru 23.\n')
+talking = input('Is the Parrot talking?y/n\n').lower()
+if talking == 'n':
     print('False')
-else:
+elif talking == 'y' and (int(hour) < 7) or (int(hour) > 20):
     print('True')
+else:
+   print('False')
+
