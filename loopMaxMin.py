cf9862e5b1c4001bd510ec1d24d27698aae325ec
millsgerald <30513476+millsgerald@users.noreply.github.com>
2017-10-02 15:54:57 +1100

    Update loopMaxMin.py

--- loopMaxMin.py
+++ loopMaxMin.py
@@ -1,3 +1,6 @@
+#rite another program that prompts for a list of numbers as 
+#above and at the end prints out both the maximum and minimum of the numbers instead of the average.
+
 count = 0
 sum = 0
 small = None
